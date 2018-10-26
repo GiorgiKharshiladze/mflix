13131c29f7700ce61afa64c688fc5392c8e9a35f
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-10-26 18:51:21 -0400

    work on durable concerns

--- mflix/db.py
+++ mflix/db.py
@@ -385,7 +385,7 @@ def get_user(email):
     """
     # TODO: User Management
     # Retrieve the user document corresponding with the user's email.
-    return db.users.find_one({"some_field": "some_value"})
+    return db.users.find_one({"email": email})
 
 
 def add_user(name, email, hashedpw):
@@ -406,7 +406,7 @@ def add_user(name, email, hashedpw):
         # Insert a user with the "name", "email", and "password" fields.
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
-        db.users.insert_one({"some_field": "some_value"})
+        db.users.insert_one({"name": name, "email":email, "password":hashedpw}, { writeConcern: { w: 1} })
         return {"success": True}
     except DuplicateKeyError:
         return {"error": "A user with the given email already exists."}
@@ -423,7 +423,7 @@ def login_user(email, jwt):
         # TODO: User Management
         # Use an UPSERT statement to update the "jwt" field in the document,
         # matching the "user_id" field with the email passed to this function.
-        db.sessions.update_one({"some_field": "some_value"}, {"$set": {"some_other_field": "some_other_value"}})
+        db.sessions.update_one({"user_id": email}, {"$set": {"jwt": jwt}}, upsert=True)
         return {"success": True}
     except Exception as e:
         return {"error": e}
@@ -439,7 +439,7 @@ def logout_user(email):
     try:
         # TODO: User Management
         # Delete the document in the `sessions` collection matching the email.
-        db.sessions.delete_one({"some_field": "some_value"})
+        db.sessions.delete_one({"user_id": email})
         return {"success": True}
     except Exception as e:
         return {"error": e}
@@ -454,7 +454,7 @@ def get_user_session(email):
     try:
         # TODO: User Management
         # Retrieve the session document corresponding with the user's email.
-        return db.sessions.find_one({"some_field": "some_value"})
+        return db.sessions.find_one({"user_id": email})
     except Exception as e:
         return {"error": e}
 
@@ -467,8 +467,8 @@ def delete_user(email):
     try:
         # TODO: User Management
         # Delete the corresponding documents from `users` and `sessions`.
-        db.sessions.delete_one({"some_field": "some_value"})
-        db.users.delete_one({"some_field": "some_value"})
+        db.sessions.delete_one({"user_id": email})
+        db.users.delete_one({"email": email})
         if get_user(email) is None:
             return {"success": True}
         else:
